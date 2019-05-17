59f924fd6433712f60af8c4e4c8be60c17cf53b0
yangminxing <ymxbtbu@163.com>
2019-05-17 11:53:33 +0800

    Summit No.2 Problem

--- src/main/java/yangminxing/leetcode/AddTwoNumber/Solution.java
+++ src/main/java/yangminxing/leetcode/AddTwoNumber/Solution.java
@@ -6,17 +6,6 @@ import java.util.List;
 class Solution {
 
     public static void main(String[] args) {
-//        ListNode a1 = new ListNode(5);
-//        ListNode a2 = new ListNode(4);
-//        ListNode a3 = new ListNode(3);
-//        a1.next = a2;
-//        a2.next = a3;
-//
-//        ListNode b1 = new ListNode(5);
-//        ListNode b2 = new ListNode(6);
-//        ListNode b3 = new ListNode(4);
-//        b1.next = b2;
-//        b2.next = b3;
         ListNode a1 = new ListNode(1);
         ListNode a2 = new ListNode(8);
         ListNode a3 = new ListNode(3);
@@ -25,9 +14,7 @@ class Solution {
 
         ListNode b1 = new ListNode(5);
         ListNode b2 = new ListNode(6);
-        ListNode b3 = new ListNode(4);
         b1.next = b2;
-        b2.next = b3;
 
         Solution s = new Solution();
         ListNode node = s.addTwoNumbers(a1, b1);
@@ -44,16 +31,15 @@ class Solution {
     }
 
     public void calTwoNumber(List<ListNode> listResult, ListNode curA, ListNode curB, int forwardNum){
-        if(forwardNum==1 && (curA == null || curB ==null)){
-            listResult.add(new ListNode(forwardNum));
-        }
-
-        if(curA == null || curB ==null){
+        if(curA == null && curB ==null){
+            if (forwardNum == 1) {
+                listResult.add(new ListNode(forwardNum));
+            }
             return;
         }
 
-        int aVal = curA.val;
-        int bVal = curB.val;
+        int aVal = curA==null?0:curA.val;
+        int bVal = curB==null?0:curB.val;
         int sumVal = aVal + bVal + forwardNum;
         int forwardSing =0;
         if(sumVal>=10){
@@ -61,7 +47,7 @@ class Solution {
             sumVal = sumVal -10;
         }
         listResult.add(new ListNode(sumVal));
-        calTwoNumber(listResult, curA.next, curB.next, forwardSing);
+        calTwoNumber(listResult, curA==null?null:curA.next, curB==null?null:curB.next, forwardSing);
     }
 }
 
